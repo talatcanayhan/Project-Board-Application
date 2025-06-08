e2ea816be125b2c5b228730b3250bd4302b8e577
talat <talatcan.ayhan@gmail.com>
2025-06-08 15:31:02 +0200

    initcommit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ProjectBoardApp.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
